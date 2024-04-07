dc62cecdf73291274c67074a7c15c0aca3354730
kakdasevemzo <matija.radovic@protonmail.com>
2024-04-07 18:16:37 +0200

    init

---
+++ tests/test_algorithm.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestAlgorithm(unittest.TestCase):
+    def test_algorithm(self) -> None:
+        assert True
---
+++ tests/test_api.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class TestApi(unittest.TestCase):
+    def test_get(self):
+        assert 200 == 200
